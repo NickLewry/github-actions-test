54041ed5607b6d2946d4c83715abc7649a29f881
NickLewry <nicholas.lewry@icloud.com>
2021-05-20 13:48:45 +0100

    trigger

--- components/ListDetail.tsx
+++ components/ListDetail.tsx
@@ -6,11 +6,12 @@ type ListDetailProps = {
   item: User;
 };
 
-const ListDetail = ({ item: user }: ListDetailProps) => (
+const ListDetail = ({ item: user }:ListDetailProps) => (
   <div>
     <h1> Detail for {user.name} </h1>
 
-    <p>ID: {user.id}</p>
+
+    <p> ID: {user.id} </p>
   </div>
 );
 
